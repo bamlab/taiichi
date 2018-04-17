1c17bc5924350c75bbb80059a40169772d5896d4
AntoineDoubovetzky <antoined@bam.tech>
2018-04-17 19:32:34 +0200

    fix(plugin-flow): fix modified_files
    affects: @bam.tech/danger-plugin-flow

--- packages/danger-plugin-flow/src/index.ts
+++ packages/danger-plugin-flow/src/index.ts
@@ -1,21 +1,21 @@
 // Provides dev-time type structures for  `danger` - doesn't affect runtime.
-import { DangerDSLType } from "../../../node_modules/danger/distribution/dsl/DangerDSL";
+import { DangerDSLType } from "../../../node_modules/danger/distribution/dsl/DangerDSL"
 
-declare var danger: DangerDSLType;
-export declare function message(message: string): void;
-export declare function warn(message: string): void;
-export declare function fail(message: string): void;
-export declare function markdown(message: string): void;
+declare var danger: DangerDSLType
+export declare function message(message: string): void
+export declare function warn(message: string): void
+export declare function fail(message: string): void
+export declare function markdown(message: string): void
 
-import * as fs from "fs";
-import * as _ from "lodash";
-import * as markdownTable from "markdown-table";
-import * as path from "path";
+import * as fs from "fs"
+import * as _ from "lodash"
+import * as markdownTable from "markdown-table"
+import * as path from "path"
 
-const STANDARD_URL = "https://bamtech.gitbooks.io/dev-standards/flowtype/flowtype.s.html";
+const STANDARD_URL = "https://bamtech.gitbooks.io/dev-standards/flowtype/flowtype.s.html"
 
 function absolute(relPath) {
-  return path.resolve(__dirname, relPath);
+  return path.resolve(__dirname, relPath)
 }
 
 // async function filter(arr, callback) {
@@ -39,46 +39,46 @@ const flowIgnorePaths = [
   "dist",
   "flow-typed",
 ].map(rel => {
-  return absolute(rel);
-});
+  return absolute(rel)
+})
 
 /* async */
 function checkForFlow(filePath) {
-  let i = 0;
-  const len = flowIgnorePaths.length;
+  let i = 0
+  const len = flowIgnorePaths.length
   while (i < len) {
-    const p = flowIgnorePaths[i];
+    const p = flowIgnorePaths[i]
     if (absolute(filePath).includes(p)) {
       // ignore this file because it's in the flow-ignore-paths.
-      return false;
+      return false
     }
-    i++;
+    i++
   }
-  const content = fs.readFileSync(filePath);
+  const content = fs.readFileSync(filePath)
   // const content = await danger.github.utils.fileContents(filePath);
-  return !content.includes("@flow");
+  return !content.includes("@flow")
 }
 
 /**
  * Ensure adoption of flow-type
  */
 export default async function flow(settings = {}) {
-  const newJsFiles = danger.git.created_files.filter(p => p.endsWith("js"));
-  const modifiedJsFiles = danger.git.created_files.filter(p => p.endsWith("js"));
+  const newJsFiles = danger.git.created_files.filter(p => p.endsWith("js"))
+  const modifiedJsFiles = danger.git.modified_files.filter(p => p.endsWith("js"))
 
   // const newUnFlowedFiles = await filter(newJsFiles, checkForFlow);
   // const modifiedUnFlowedFiles = await filter(modifiedJsFiles, checkForFlow);
 
-  const newUnFlowedFiles = newJsFiles.filter(checkForFlow);
-  const modifiedUnFlowedFiles = modifiedJsFiles.filter(checkForFlow);
+  const newUnFlowedFiles = newJsFiles.filter(checkForFlow)
+  const modifiedUnFlowedFiles = modifiedJsFiles.filter(checkForFlow)
 
-  const unFlowedFiles = [...newUnFlowedFiles, ...modifiedUnFlowedFiles];
+  const unFlowedFiles = [...newUnFlowedFiles, ...modifiedUnFlowedFiles]
 
-  const failOrWarn = newUnFlowedFiles.length > 0 ? fail : warn;
-  const newOrExisting = newUnFlowedFiles.length > 0 ? "New" : "Existing";
+  const failOrWarn = newUnFlowedFiles.length > 0 ? fail : warn
+  const newOrExisting = newUnFlowedFiles.length > 0 ? "New" : "Existing"
 
   if (unFlowedFiles.length > 0) {
-    failOrWarn(`${newOrExisting} files have not @flow enabled`);
+    failOrWarn(`${newOrExisting} files have not @flow enabled`)
     markdown(`#### Explanations for \`${newOrExisting} files have not @flow enabled\`
 
 <details>
@@ -107,6 +107,6 @@ flow to give your error when it would not do.
 
 </p>
 </details>
-  `);
+  `)
   }
 }
